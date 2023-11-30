7d931fd326e57dde5b2e4cf2ce2cbb2f8643ab87
GermanBread <work+contact@breadwas.uber.space>
2023-11-30 09:54:11 +0100

    update inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Declarative flatpaks.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     utils.url = "github:numtide/flake-utils";
   };
 
