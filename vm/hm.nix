cf52d112cbe73d823903592a8500fc2ad6ec7031
GermanBread <git@breadwas.uber.space>
2024-08-24 17:50:35 +0200

    update vm config

--- vm/hm.nix
+++ vm/hm.nix
@@ -9,18 +9,14 @@
 
     services.flatpak = {
       packages = [
-        "flathub:app/de.shorsh.discord-screenaudio//stable"
-        # "flathub-beta:app/org.chromium.Chromium//beta"
-        # "flathub:app/com.usebottles.bottles//stable"
+        "flathub-beta:app/org.chromium.Chromium//beta"
+        "flathub:app/com.usebottles.bottles//stable"
       ];
       remotes = {
         "flathub" = "https://flathub.org/repo/flathub.flatpakrepo";
         "flathub-beta" = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
       };
       enable-debug = true;
-      preRemotesCommand = "echo silly1";
-      preInstallCommand = "echo silly2";
-      preSwitchCommand = "echo silly4";
       state-dir = "${config.home.homeDirectory}/flatpak/state";
       target-dir = "${config.home.homeDirectory}/flatpak/deploy";
     };
