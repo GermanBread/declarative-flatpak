65d091f157483e51b3a85dab7f14fe999cd8ce0d
Emma <contact@emma.pizza>
2026-08-11 01:34:15 +0200

    fix issue with custom commits

--- module/script.nix
+++ module/script.nix
@@ -219,7 +219,7 @@ let
           _id="$(<"$ref"/id)"
           _commit="$(<"$ref"/commit)"
 
-          if ! flatpak update --commit="$_commit" "$_id"; then
+          if ! flatpak update --noninteractive --commit="$_commit" "$_id"; then
             echo "Failed to update ref $_id to commit $_commit. Verify if the commit is correct"
             exit 1
           fi
